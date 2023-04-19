e29a893e8aceefb8b93d8587131f1f50812cd5d4
ChrisBlassiaux <chrisblassiaux@gmail.com>
2023-04-19 16:42:52 +0200

    init project

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="navbar-height"></div>
+    <div class="navbar">
+      <div class="contain-burger">
+        <div class="burger"></div>
+      </div>
+      <nav class="nav">
+        <img src="../assets/logotype.png" alt="Logotype" />
+        <a href="#" class="navbar-link active">Accueil</a>
+      </nav>
+    </div>
+    <nav class="vertical-navbar">
+      <a href="#" class="nav-vertical-link">
+        <span class="fas fa-globe fa-3x"></span>
+        <p>Planètes</p>
+      </a>
+      <a href="#" class="nav-vertical-link">
+        <span class="fas fa-rocket fa-3x"></span>
+        <p>Véhicules</p>
+      </a>
+      <a href="#" class="nav-vertical-link">
+        <span class="fab fa-reddit-alien fa-3x"></span>
+        <p>Personnes</p>
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/HomePageView.vue
@@ -0,0 +1,16 @@
+<template>
+    <main class="container container-1-1">
+        <div>
+            <div class="card">
+            </div>
+            <div class="card">
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'HomePageView'
+    }
+</script>
